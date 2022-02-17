17b21719698776945a1a59744cc11c794c51b958
ARMAN-8 <alimnurarman@gmail.com>
2022-02-17 11:29:23 +0600

    login page is done

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="Login h-screen flex items-center justify-center">
-    <div class="card bg-gray-100 shadow-2xl rounded-2xl text-center p-6 rounded-lg space-y-6">
-      <div class="">
-        <p class="block">
-          <b>Войдите</b> в учетную запись
+    <div class="card text-center p-6 space-y-6">
+      <div class="space-y-6">
+        <p class="signInTxt block">
+          <span>Войдите</span> в учетную запись
         </p>
         <input type="email" placeholder="Введите почту"
-               class="block w-full border-2 rounded-lg px-4 py-2"/>
+               class="input block w-full px-4 py-2"/>
         <input type="password" placeholder="Введите пароль"
-               class="block w-full border-2 rounded-lg px-4 py-2"/>
-        <button class="bg-green-500 text-white w-full py-2 rounded-lg">
-          Войти
+               class="input block w-full px-4 py-2"/>
+        <button class="signInBtn text-white w-full py-2 rounded-lg">
+          <span>Войти</span>
         </button>
         <div class="flex justify-between">
-          <span>Запомнить меня</span>
-          <a href="#">Забыли пароль?</a>
+          <span class="chBoxRemember">Запомнить меня</span>
+          <a class="forgetPsw" href="#">Забыли пароль?</a>
         </div>
       </div>
     </div>
@@ -25,3 +25,61 @@ export default {
   name: 'Login'
 }
 </script>
+<style scoped>
+
+.card {
+  width: 304px;
+  border-radius: 10px;
+  background: #FFFFFF;
+  box-shadow: 0px 12px 19px 1px rgba(189, 189, 189, 0.13);
+}
+
+.signInTxt {
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 18.96px;
+  color: #000000;
+}
+
+.signInTxt span {
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 18.96px;
+  color: #000000;
+}
+
+.input {
+  border: 1px solid #ececec;
+  border-radius: 3px;
+}
+
+.chBoxRemember {
+  font-weight: 300;
+  color: #2c3340;
+  font-size: 12px;
+  line-height: 14px;
+}
+
+.forgetPsw {
+  color: #2568ef;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 14px;
+}
+
+.signInBtn {
+  background-color: #0BA360;
+  border-radius: 3px;
+}
+.signInBtn:hover {
+  background-color: #068a4f;
+  border-radius: 3px;
+}
+
+.signInBtn span {
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 14px;
+  color: #ffffff;
+}
+</style>
